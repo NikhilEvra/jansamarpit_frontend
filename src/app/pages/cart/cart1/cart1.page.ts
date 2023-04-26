a9d9707b56fd2fbc254b9e5f7e87513202143fc2
NikhilEvra <nikhil@evramedia.com>
2023-04-26 17:30:10 +0530

    cart add and hit api

--- src/app/pages/cart/cart1/cart1.page.ts
+++ src/app/pages/cart/cart1/cart1.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import { CartService } from 'src/app/service/cart/cart.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-cart1',
@@ -12,11 +15,22 @@ export class Cart1Page implements OnInit {
   getuserdata:any=[];
 
   form!: FormGroup
- model:any=[];
- model_name:any=[];
+  model:any=[];
+  model_name:any=[];
+  response:any=[];
+  myfun = false;
+  priceWithbatt!:any;
+  priceWithOutbatt!:any;
+  amount!:any;
+  amount2!:any;
+  total!:any;
+ 
   constructor(
     private route : ActivatedRoute,
-    private formb : FormBuilder
+    private formb : FormBuilder,
+    private api : CartService,
+    private router : Router,
+    private loadingCtrl : LoadingController
   ) { console.log(this.USTEMP);
     if (this.USTEMP) {
       this.getuserdata = JSON.parse(this.USTEMP) ;
@@ -24,26 +38,86 @@ export class Cart1Page implements OnInit {
 
   Initform(){
     this.form = this.formb.group({    
-     name: [this.getuserdata.id, Validators.required],
+     dealerid: [this.getuserdata.id, Validators.required],
      model : [this.model_name,Validators.required],
-     color : ['',Validators.required]
+     color : ['',Validators.required],
+     quantity_with_batt: ['',Validators.required],
+     quantity_without_batt:['',Validators.required],
+    //  totalamount:[this.total, Validators.required],
+    amountWithOutBatt:[this.amount2],
+    amountWithBatt:[this.amount],
     })
-
-   
   }
   ngOnInit() {
     this.route.queryParams
     .subscribe(params => {
       console.log(params); 
-      // this.orderby = params.orderby;
-      // console.log(this.orderby); // price
       this.model = params;
-     
       this.model_name = this.model.model;
     }
   );
   
   this.Initform();
+  this.getdata();
   }
 
+  getdata(){
+    this.showLoading();
+    this.api.getVarients(this.model_name).subscribe({
+      next:(data) =>{
+        console.log(data);
+        this.response = data;
+       
+     
+      },
+      error:() =>{
+        alert('error');
+     
+      },
+      complete:() =>{
+        // this.loadingCtrl.dismiss();
+      }
+    })
+    
+  }
+
+  show(){
+    this.myfun = true;
+  }
+  calAmount(){
+   this.amount = this.priceWithbatt * 100000;
+  }
+  calAmount2(){
+    this.amount2 = this.priceWithOutbatt * 90000;
+   }
+
+   submit(){
+    this.showLoading();
+    console.log(this.form.value);  
+    this.api.postCartData(this.form.value.dealerid,this.form.value.model,this.form.value.color,this.form.value.quantity_with_batt,this.form.value.quantity_without_batt,this.form.value.amountWithBatt,this.form.value.amountWithOutBatt).subscribe({
+      next:(data) => {
+        console.log(data);
+        this.response = data;
+      },
+      error:() => {
+        console.log('err');
+         Swal.fire({'imageUrl' :'assets/icon/login.gif','imageHeight':'100px', 'title': 'Internal Server Error!',  heightAuto: false ,  timer: 3000});
+      },
+      complete:() => {
+   
+        this.router.navigateByUrl('/po');
+        Swal.fire({'imageUrl' :'assets/icon/login.gif','imageHeight':'100px', 'title': this.response.message,  heightAuto: false ,  timer: 3000});
+        // this.loadingCtrl.dismiss();
+      }
+    })
+   }
+
+   async showLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading please Wait...',
+      duration: 2000,
+    });
+
+    loading.present();
+  }
 }
--- src/app/service/cart/cart.service.ts
+++ src/app/service/cart/cart.service.ts
@@ -10,5 +10,21 @@ export class CartService {
 
   constructor(private api : HttpClient) { }
 
+  getVarients(p_id : any):Observable<any>{
+    return this.api.get<any>(environment.apiurl + 'get_varients.php?p_id=' + p_id);
+  }
+
+  postCartData(dealerid:any, model:any, color: any, quantity_with_batt : any, quantity_withOut_batt: any, amountWithOutBatt:any,amountWithBatt: any):Observable<any>{
+    const formData = new FormData();
+    formData.append('dealerid', dealerid);
+    formData.append('model', model);
+    formData.append('color', color);
+    formData.append('quantity_with_batt', quantity_with_batt);
+    formData.append('quantity_without_batt', quantity_withOut_batt);
+    formData.append('amountWithBatt', amountWithBatt);
+    formData.append('amountWithOutBatt', amountWithOutBatt);
+    
+    return this.api.post<any>(environment.apiurl + 'add_cart.php',  formData);
+  }
 
 }
